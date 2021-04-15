88b19fd52886581f19f826ba74424394f9d23df1
dzbz0373 <thomas.thebaud@orange.com>
2021-04-15 18:35:22 +0200

    small changes

--- run.sh
+++ run.sh
@@ -91,7 +91,7 @@ if [ $stage -le 1 ]; then
     --emb_src $expe_dir/Emb_U.npy \
     --label_src $expe_dir/User_U.npy \
     --emb_tgt $expe_dir/Emb_L.npy \
-    --label_tgt $expe_dir/User_L.npy
+    --label_tgt $expe_dir/User_L.npy \
     --rotation $expe_dir/WP_R.npy
 
   printf "${GREEN}Done${NC}\n"
@@ -119,8 +119,8 @@ if [ $stage -le 2 ]; then
       --emb_src $expe_dir/Emb_U.npy \
       --label_src $expe_dir/User_U.npy \
       --emb_tgt $expe_dir/Emb_L.npy \
-      --label_tgt $expe_dir/User_L.npy
-      --rotation $expe_dir/WP_R.npy
+      --label_tgt $expe_dir/User_L.npy \
+      --rotation $expe_dir/WP_R.npy \
       --test
 
   done
